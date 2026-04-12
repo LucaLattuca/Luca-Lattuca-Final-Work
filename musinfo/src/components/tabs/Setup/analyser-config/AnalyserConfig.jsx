89c6a292541aaa690e2f8427d1fcc0f99f5f501a
LucaLattuca <luca.lattuca@student.ehb.be>
2026-04-12 17:42:40 +0200

    feat: add SessionContext to share analyser selection across components

--- musinfo/src/components/tabs/Setup/analyser-config/AnalyserConfig.jsx
+++ musinfo/src/components/tabs/Setup/analyser-config/AnalyserConfig.jsx
@@ -1,27 +1,34 @@
-import { useState } from 'react';
-import React from 'react';
+import { useSession } from '../../../../contexts/SessionContext';
 import styles from './AnalyserConfig.module.css';
 
 const AnalyserConfig = () => {
-
+  const { session, updateAnalyser } = useSession();
 
   return (
     <div className={styles.analyserConfig}>
-        <h2>Analyser Selection</h2>
-        <ul className={styles.analyserList}>
-          <li>
-            <label>
-              <input type="checkbox" name="genreAnalyser" />
-              Genre Analyser
-            </label>
-          </li>
-          <li>
-            <label>
-              <input type="checkbox" name="noteAnalyser" />
-              Note Analyser
+      <h2>Analyser Selection</h2>
+      <ul className={styles.analyserList}>
+        <li>
+          <label>
+            <input
+              type="checkbox"
+              checked={session.analysers.genre}
+              onChange={(e) => updateAnalyser("genre", e.target.checked)}
+            />
+            Genre Analyser
           </label>
         </li>
-        </ul>
+        <li>
+          <label>
+            <input
+              type="checkbox"
+              checked={session.analysers.lyrics}
+              onChange={(e) => updateAnalyser("lyrics", e.target.checked)}
+            />
+            Lyrics Analyser
+          </label>
+        </li>
+      </ul>
     </div>
   );
 };
