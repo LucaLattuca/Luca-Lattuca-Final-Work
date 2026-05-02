375f9e9fb5c4f9951fae00eb4f930825dfadcc6d
LucaLattuca <luca.lattuca@student.ehb.be>
2026-05-02 16:12:12 +0200

    fix: Sidebar.jsx replace static JSON import with instruments prop

--- musinfo/src/components/layout/Sidebar.jsx
+++ musinfo/src/components/layout/Sidebar.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
 import styles from './Sidebar.module.css';
-import instruments from '../../../backend/config/instruments.json';
 
 
-const Sidebar = ({ onAddInstrument, onSelectInstrument, selectedInstrument}) => {
+const Sidebar = ({ onAddInstrument, onSelectInstrument, selectedInstrument, instruments}) => {
   return (
     <div className={styles.sidebar}>
-      {/* <p>No instruments added.</p> */}
       <div className={styles.instrumentList}>
         <p className={styles.sidebarTitle}>Instruments</p>
       
-          {Object.entries(instruments.instruments).map(([name, data]) => (
-              <div  
-                className={`${styles.instrumentcard} ${selectedInstrument?.name === name ? styles.selectedInstrument : ''}`}
-                key={name} 
-                onClick={() => onSelectInstrument(name, data)}
-              >
-                
-                <h3>{name}</h3>
-                <p>{data.audio_device.name} - Ch.{data.audio_device.channel + 1}</p>
-                
-              </div>
-          ))}
+        {Object.entries(instruments).map(([name, data]) => (
+          <div
+            key={name}
+            className={`${styles.instrumentcard} ${selectedInstrument?.name === name ? styles.selectedInstrument : ''}`}
+            onClick={() => onSelectInstrument(name, data)}
+          >
+            <h3>{name}</h3>
+            <p>{data.audio_device.name} — Ch.{data.audio_device.channel + 1}</p>
+          </div>
+        ))}
         
         <p className={styles.sidebarTitle}>Mix</p>
         <div className={styles.mix}>
           {/* ADD Instrument role */}
         </div>
       </div>
+
+      
       <div className={styles.addInstrument}>
         <button onClick={onAddInstrument} className={styles.addInstrumentButton}>
           Add Instruments
