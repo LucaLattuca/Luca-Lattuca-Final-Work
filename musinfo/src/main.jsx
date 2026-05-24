61351c4c4218a1a68fde35e7ae423ecdbc83cace
LucaLattuca <luca.lattuca@student.ehb.be>
2026-05-24 19:55:59 +0200

    Refactor: remove unused SessionContext

--- musinfo/src/main.jsx
+++ musinfo/src/main.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { SessionProvider } from "./contexts/SessionContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <SessionProvider>
       <App />
-    </SessionProvider>
   </React.StrictMode>,
 );
